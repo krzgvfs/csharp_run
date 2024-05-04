456d6c7e5e92b23cc24efc10cbf1342c03ab7781
resux94 <resux94@DESKTOP-RSM46LG>
2024-05-04 17:14:53 -0300

    Add project files.

---
+++ HelloWordl/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace HelloWordl	// Conjunto de classes
+{	
+	internal class Program	// Classe principal
+	{	
+		static void Main(string[] args)	// Método principal
+		{
+			ExecutarComandoPowerShell();
+		}
+
+
+		public static void ExecutarComandoPowerShell()
+		{
+			string caminhoDoScript = "C:\\Users\\resux94\\source\\repos\\HelloWordl\\HelloWordl\\Scripts\\searchbar.ps1";
+
+			var starInfo = new ProcessStartInfo {
+				FileName = "powershell.exe",
+				Arguments = $"-NoProfile -ExecutionPolicy Bypass -File \"{caminhoDoScript}\"",
+				UseShellExecute = false
+			};
+			Process.Start(starInfo);
+		}
+	}
+}
---
+++ HelloWordl/Scripts/searchbar.ps1
@@ -0,0 +1,2 @@
+﻿Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "SearchboxTaskbarMode" -Value 2
+Stop-Process -Name explorer -Force
